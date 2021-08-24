f85f0b6caa3c738399350d4f304d197339c9bbc7
climb719 <mary.oldweiler@gmail.com>
2021-08-24 13:53:38 -0400

    commit tree 7af276a9a1fc9e1d95e68ff538de74162a103c2a

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Your code here
+class Polygon {
+    constructor(sides) {
+    this.sides = sides
+    this.count = this.sides.length
+    }
+
+    get countSides() {
+        return this.count
+    }
+
+    get perimeter() {
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        return this.sides.reduce(reducer)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        let side1 = this.sides[0]
+        let side2 = this.sides[1]
+        let side3 = this.sides[2]
+        if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
+            //console.log("I'm a traingle!")
+            return true
+        } else { return false
+        }
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        let side1 = this.sides[0]
+        let side2 = this.sides[1]
+        let side3 = this.sides[2]
+        let side4 = this.sides[3]
+        if (side1 === side2 && side2 === side3 && side3 === side4) {
+            console.log("I'm a square")
+            return true
+        } else { return false
+       }
+    }
+
+    get area() {
+        return this.sides[0] * this.sides[1]
+    }
+}
+
